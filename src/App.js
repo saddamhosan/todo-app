4b7bce41d23488fd938703c654b82aa4aaaa2773
saddamhosan <contact.saddamhosan.com>
2022-06-30 14:35:16 +0600

    added route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import Calendar from "./component/Calendar";
+import Completed from "./component/Completed";
 import Home from "./component/Home";
+import Menu from "./Menu";
 
 
 function App() {
   return (
     <div>
+      <Menu/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/calendar" element={<Calendar/>}/>
+        <Route path="/completed" element={<Completed/>}/>
       </Routes>
     </div>
   );
---
+++ src/Menu.js
@@ -0,0 +1,60 @@
+import { Link, NavLink } from "react-router-dom";
+
+const Menu = () => {
+    return (
+      <div class="navbar bg-base-100">
+        <div class="navbar-start">
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabindex="0"
+              class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/calendar">Calendar</NavLink>
+              </li>
+              <li>
+                <NavLink to="/completed">Completed</NavLink>
+              </li>
+            </ul>
+          </div>
+          <Link to="/" class="btn btn-ghost normal-case text-xl">
+            To-Do
+          </Link>
+        </div>
+        <div class="navbar-end hidden lg:flex">
+          <ul class="menu menu-horizontal p-0">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/calendar">Calendar</NavLink>
+            </li>
+            <li>
+              <NavLink to="/completed">Completed</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+};
+
+export default Menu;
